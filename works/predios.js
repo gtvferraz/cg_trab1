f0b1e19290ced3f72ef30f4d8e13782aebea5292
Vinicius <recepute200@hotmail.com>
2021-08-28 14:29:12 -0300

    predio01 finalizado

--- works/predios.js
+++ works/predios.js
@@ -21,158 +21,13 @@ scene.add(ambientLight);
 let sunLight = initLight(scene, new THREE.Vector3(-30,0,30)); 
 
 const textureLoader = new THREE.TextureLoader();
-//tiles_stone_001
-const tilesBaseColor = textureLoader.load('./assets/Tiles_Stone_001_basecolor.jpg');
-const tilesNormalMap = textureLoader.load('./assets/Tiles_Stone_001_normal.jpg');
-const tilesHeightMap = textureLoader.load('./assets/Tiles_Stone_001_height.png');
-const tilesRoughnessMap = textureLoader.load('./assets/Tiles_Stone_001_roughness.jpg');
-const tilesAmbientOcclusionMap = textureLoader.load('./assets/Tiles_Stone_001_ambientOcclusion.jpg');
-//wood_window_001
-/*
-const woodWindowBaseColor = textureLoader.load('./assets/Wood_Window_001_basecolor.jpg');
-const woodWindowNormalMap = textureLoader.load('./assets/Wood_Window_001_normal.jpg');
-const woodWindowHeightMap = textureLoader.load('./assets/Wood_Window_001_height.png');
-const woodWindowRoughnessMap = textureLoader.load('./assets/Wood_Window_001_roughness.jpg');
-const woodWindowAmbientOcclusionMap = textureLoader.load('./assets/Wood_Window_001_ambientOcclusion.jpg');
-const woodWindowMetallicMap = textureLoader.load('./assets/Wood_Window_001_metallic.jpg');
-const woodWindowOpacityMap = textureLoader.load('./assets/Wood_Window_001_opacity.jpg');*/
-//glass_window_003
-const glassBaseColor = textureLoader.load('./assets/Glass_Window_003_basecolor.jpg');
-const glassNormalMap = textureLoader.load('./assets/Glass_Window_003_normal.jpg');
-const glassHeightMap = textureLoader.load('./assets/Glass_Window_003_height.png');
-const glassRoughnessMap = textureLoader.load('./assets/Glass_Window_003_roughness.jpg');
-const glassAmbientOcclusionMap = textureLoader.load('./assets/Glass_Window_003_ambientOcclusion.jpg');
-const glassMetallicMap = textureLoader.load('./assets/Glass_Window_003_metallic.jpg');
-const glassOpacityMap = textureLoader.load('./assets/Glass_Window_003_opacity.jpg');
-//door_wood_001
-const doorWoodBaseColor = textureLoader.load('./assets/Door_Wood_001_basecolor.jpg');
-const doorWoodNormalMap = textureLoader.load('./assets/Door_Wood_001_normal.jpg');
-const doorWoodHeightMap = textureLoader.load('./assets/Door_Wood_001_height.png');
-const doorWoodRoughnessMap = textureLoader.load('./assets/Door_Wood_001_roughness.jpg');
-const doorWoodAmbientOcclusionMap = textureLoader.load('./assets/Door_Wood_001_ambientOcclusion.jpg');
-const doorWoodMetallicMap = textureLoader.load('./assets/Door_Wood_001_metallic.jpg');
-const doorWoodOpacityMap = textureLoader.load('./assets/Door_Wood_001_opacity.jpg');
-//door
-const portaMap = textureLoader.load('./assets/porta.png')
-//geometry
-var gradeFrenteGeometry = new THREE.BoxGeometry(9,0.1,0.1);
-var gradeLateralGeometry = new THREE.BoxGeometry(0.1,3,0.1);
-var pisoGeometry = new THREE.PlaneGeometry(9, 3);
-var portaVarandaGeometry = new THREE.PlaneGeometry(1,2.5);
-var predioGeometry = new THREE.BoxGeometry(20, 8, 20, 512, 512);
-var pilarGeometry = new THREE.BoxGeometry(2,3,16);
-var janelaGeometry = new THREE.PlaneGeometry(1,1, 512, 512);
-
-//material
-var pisoMaterial = new THREE.MeshBasicMaterial({
-    color: "rgba(150, 150, 150)",
-    side: THREE.DoubleSide,
-});
-var portaVarandaMaterial = new THREE.MeshLambertMaterial({color:"rgb(255,255,255)",side:THREE.DoubleSide});
-portaVarandaMaterial.map = portaMap
-/*var portaVarandaMaterial = new THREE.MeshPhysicalMaterial({
-    map: doorWoodBaseColor,
-    normalMap: doorWoodNormalMap,
-    displacementMap: doorWoodHeightMap,
-    displacementScale: 0.05,
-    roughnessMap: doorWoodRoughnessMap,
-    roughness: 0,
-    aoMap: doorWoodAmbientOcclusionMap,
-    metalnessMap: doorWoodMetallicMap,
-    metalness: 1,
-    alphaMap: doorWoodOpacityMap,
-});
-portaVarandaMaterial.alphaTest = 0.11;
-portaVarandaGeometry.attributes.uv2 = portaVarandaGeometry.attributes.uv;*/
-var gradeMaterial = new THREE.MeshBasicMaterial();
-var cubeMaterial = new THREE.MeshNormalMaterial();
-var janelaMaterial = new THREE.MeshPhysicalMaterial({
-    map: glassBaseColor,
-    normalMap: glassNormalMap,
-    displacementMap: glassHeightMap,
-    displacementScale: 0.05,
-    roughnessMap: glassRoughnessMap,
-    roughness: 0,
-    aoMap: glassAmbientOcclusionMap,
-    metalnessMap: glassMetallicMap,
-    metalness: 1,
-    alphaMap: glassOpacityMap,
-});
-janelaMaterial.alphaTest = 0.11;
-janelaGeometry.attributes.uv2 = janelaGeometry.attributes.uv;
-var predioMaterial = new THREE.MeshStandardMaterial({
-    map: tilesBaseColor,
-    normalMap: tilesNormalMap,
-    displacementMap: tilesHeightMap,
-    displacementScale: 0.001,
-    roughnessMap: tilesRoughnessMap,
-    roughness: 0.1,
-    aoMap: tilesAmbientOcclusionMap
-});
-predioGeometry.attributes.uv2 = predioGeometry.attributes.uv;
-
-var predio = new THREE.Mesh(predioGeometry, predioMaterial);
-predio.position.set(0,0,10);
-scene.add(predio);
-
-var pilar = new THREE.Mesh(pilarGeometry, cubeMaterial);
-predio.add(pilar);
-pilar.position.set(0,-4 -1.5,2)
-
-for(var i = 0; i<5; i++) {
-    var gradeFrenteEsq = new THREE.Mesh(gradeFrenteGeometry, gradeMaterial);
-    var gradeFrenteDir = new THREE.Mesh(gradeFrenteGeometry, gradeMaterial);
-    predio.add(gradeFrenteEsq);
-    predio.add(gradeFrenteDir);
-    gradeFrenteEsq.position.set(-5.5, -4 -3, -5 + i*3.2);
-    gradeFrenteDir.position.set(5.5, -4 -3, -5 + i*3.2);
-
-    var gradeLateral1 = new THREE.Mesh(gradeLateralGeometry, gradeMaterial);
-    var gradeLateral2 = new THREE.Mesh(gradeLateralGeometry, gradeMaterial);
-    predio.add(gradeLateral1);
-    predio.add(gradeLateral2);
-    gradeLateral1.position.set(-10 + 0.05, -4 - 1.5, -5 + i*3.2);
-    gradeLateral2.position.set(10 - 0.05, -4 - 1.5, -5 + i*3.2)
-
-    var pisoEsq = new THREE.Mesh(pisoGeometry, pisoMaterial);
-    var pisoDir = new THREE.Mesh(pisoGeometry,pisoMaterial);
-    predio.add(pisoEsq);
-    predio.add(pisoDir);
-    pisoEsq.position.set(-5.5, -4 - 1.5, -6 + i*3.2)
-    pisoDir.position.set(5.5, -4 - 1.5, -6 + i*3.2)
-
-    var portaVarandaEsq = new THREE.Mesh(portaVarandaGeometry, portaVarandaMaterial);
-    var portaVarandaDir = new THREE.Mesh(portaVarandaGeometry, portaVarandaMaterial);
-    predio.add(portaVarandaEsq);
-    predio.add(portaVarandaDir);
-    portaVarandaEsq.rotateOnAxis(new THREE.Vector3(1,0,0), degreesToRadians(90));
-    portaVarandaDir.rotateOnAxis(new THREE.Vector3(1,0,0), degreesToRadians(90));
-    portaVarandaEsq.position.set(-1.5, -4.01, -6 + 2.5/2 + i*3.2)
-    portaVarandaDir.position.set(1.5, -4.01, -6 + 2.5/2 + i*3.2)
-
-    var janelaEsq = new THREE.Mesh(janelaGeometry, janelaMaterial);
-    var janelaDir = new THREE.Mesh(janelaGeometry, janelaMaterial);
-    predio.add(janelaEsq);
-    predio.add(janelaDir);
-    janelaEsq.rotateOnAxis(new THREE.Vector3(0,1,0), degreesToRadians(-90));
-    janelaDir.rotateOnAxis(new THREE.Vector3(0,1,0), degreesToRadians(90));
-    janelaEsq.position.set(-10.01, 0, -4.5 + i*3.2)
-    janelaDir.position.set(10.01, 0, -4 + i*3.2)
-}
-
-var portaEntradaGeometry = new THREE.PlaneGeometry(5,4);
-var portaMaterial = new THREE.MeshBasicMaterial();
-var porta = new THREE.Mesh(portaEntradaGeometry,portaMaterial);
-predio.add(porta);
-porta.rotateOnAxis(new THREE.Vector3(1,0,0), degreesToRadians(90));
-porta.position.set(0,-4.01,-8)
-
 
 // Show axes (parameter is size of each axis)
 var axesHelper = new THREE.AxesHelper(12)
 scene.add(axesHelper);
 // Enable mouse rotation, pan, zoom etc.
 var trackballControls = new TrackballControls(camera, renderer.domElement);
+predio01(new THREE.Vector3(0,0,0), textureLoader);
 render();
 
 function setSpotLight(position,spotLight)
@@ -186,6 +41,167 @@ function setSpotLight(position,spotLight)
   spotLight.penumbra = 0.5;
   spotLight.name = "Spot Light"
   scene.add(spotLight);
+}
+
+function predio01(position, textureLoader) {
+    //tiles_stone_001
+    const tilesBaseColor = textureLoader.load('./assets/predio_01/Tiles_Stone_001_basecolor.jpg');
+    const tilesNormalMap = textureLoader.load('./assets/predio_01/Tiles_Stone_001_normal.jpg');
+    const tilesHeightMap = textureLoader.load('./assets/predio_01/Tiles_Stone_001_height.png');
+    const tilesRoughnessMap = textureLoader.load('./assets/predio_01/Tiles_Stone_001_roughness.jpg');
+    const tilesAmbientOcclusionMap = textureLoader.load('./assets/predio_01/Tiles_Stone_001_ambientOcclusion.jpg');
+    //brick_wall_015
+    const brickWallBaseColor = textureLoader.load('./assets/predio_01/Brick_Wall_015_COLOR.jpg');
+    const brickWallNormalMap = textureLoader.load('./assets/predio_01/Brick_Wall_015_NORM.jpg');
+    const brickWallHeightMap = textureLoader.load('./assets/predio_01/Brick_Wall_015_DISP.png');
+    const brickWallRoughnessMap = textureLoader.load('./assets/predio_01/Brick_Wall_015_ROUGH.jpg');
+    const brickWallAmbientOcclusionMap = textureLoader.load('./assets/predio_01/Brick_Wall_015_OCC.jpg');
+    //window_001
+    const WindowBaseColor = textureLoader.load('./assets/predio_01/Window_001_basecolor.jpg');
+    const WindowNormalMap = textureLoader.load('./assets/predio_01/Window_001_normal.jpg');
+    const WindowHeightMap = textureLoader.load('./assets/predio_01/Window_001_height.png');
+    const WindowRoughnessMap = textureLoader.load('./assets/predio_01/Window_001_roughness.jpg');
+    const WindowAmbientOcclusionMap = textureLoader.load('./assets/predio_01/Window_001_ambientOcclusion.jpg');
+    const WindowMetallicMap = textureLoader.load('./assets/predio_01/Window_001_metallic.jpg');
+    const WindowOpacityMap = textureLoader.load('./assets/predio_01/Window_001_opacity.jpg');
+
+    //porta varanda
+    const portaMap = textureLoader.load('./assets/predio_01/porta.png');
+    const portaMap2 = textureLoader.load('./assets/predio_01/porta.png');
+    //chão varanda
+    const varandaMap = textureLoader.load('./assets/predio_01/Marble_Carrara_003_COLOR.jpg');
+    //grade
+    const gradeMap = textureLoader.load('./assets/predio_01/Wood_Herringbone_Tiles_002_basecolor.jpg');
+    //porta entrada
+    const portaEntradaMap = textureLoader.load('./assets/predio_01/porta2.jpg');
+
+    //geometry
+    var gradeFrenteGeometry = new THREE.BoxGeometry(9, 0.1, 0.1);
+    var gradeLateralGeometry = new THREE.BoxGeometry(0.1, 3, 0.1);
+    var pisoGeometry = new THREE.BoxGeometry(9, 3, 0.2);
+    var portaVarandaGeometry = new THREE.PlaneGeometry(1, 2.5);
+    var predioGeometry = new THREE.BoxGeometry(20, 8, 20, 512, 512);
+    var pilarGeometry = new THREE.BoxGeometry(2, 3, 16, 512, 512);
+    var janelaGeometry = new THREE.PlaneGeometry(1, 1, 512, 512);
+    var janelaFrenteGeometry = new THREE.PlaneGeometry(4, 1.5, 512, 512)
+    var portaFrenteGeometry = new THREE.PlaneGeometry(5,4);
+
+    //material
+    var pisoMaterial = new THREE.MeshBasicMaterial({
+        color: "rgba(150, 150, 150)",
+        side: THREE.DoubleSide,
+    });
+    pisoMaterial.map = varandaMap;
+    var portaVarandaMaterial = new THREE.MeshLambertMaterial({color:"rgb(255,255,255)"});
+    portaVarandaMaterial.map = portaMap;
+    var portaVarandaMaterial2 = new THREE.MeshLambertMaterial({color:"rgb(255,255,255)"});
+    portaVarandaMaterial2.map = portaMap2;
+    var portaFrenteMaterial = new THREE.MeshLambertMaterial({color:"rgb(255,255,255)"});
+    portaFrenteMaterial.map = portaEntradaMap;
+
+    var gradeMaterial = new THREE.MeshLambertMaterial({color:"rgb(255,255,255)"});
+    gradeMaterial.map = gradeMap;
+    var janelaMaterial = new THREE.MeshPhysicalMaterial({
+        map: WindowBaseColor,
+        normalMap: WindowNormalMap,
+        displacementMap: WindowHeightMap,
+        displacementScale: 0.05,
+        roughnessMap: WindowRoughnessMap,
+        roughness: 0,
+        aoMap: WindowAmbientOcclusionMap,
+        metalnessMap: WindowMetallicMap,
+        metalness: 1,
+        alphaMap: WindowOpacityMap
+    });
+    janelaMaterial.alphaTest = 0.01;
+    janelaGeometry.attributes.uv2 = janelaGeometry.attributes.uv;
+    var predioMaterial = new THREE.MeshStandardMaterial({
+        map: tilesBaseColor,
+        normalMap: tilesNormalMap,
+        displacementMap: tilesHeightMap,
+        displacementScale: 0.001,
+        roughnessMap: tilesRoughnessMap,
+        roughness: 0.1,
+        aoMap: tilesAmbientOcclusionMap
+    });
+    predioGeometry.attributes.uv2 = predioGeometry.attributes.uv;
+    var pilarMaterial = new THREE.MeshStandardMaterial({
+        map: brickWallBaseColor,
+        normalMap: brickWallNormalMap,
+        displacementMap: brickWallHeightMap,
+        displacementScale: 0.001,
+        roughnessMap: brickWallRoughnessMap,
+        roughness: 0.1,
+        aoMap: brickWallAmbientOcclusionMap
+    });
+    pilarGeometry.attributes.uv2 = pilarGeometry.attributes.uv;
+
+    var predio = new THREE.Mesh(predioGeometry, predioMaterial);
+    predio.position.copy(position);
+    scene.add(predio);
+
+    var pilar = new THREE.Mesh(pilarGeometry, pilarMaterial);
+    predio.add(pilar);
+    pilar.position.set(0,-4 -1.5,2);
+
+    for(var i = 0; i<5; i++) {
+        var gradeFrenteEsq = new THREE.Mesh(gradeFrenteGeometry, gradeMaterial);
+        var gradeFrenteDir = new THREE.Mesh(gradeFrenteGeometry, gradeMaterial);
+        predio.add(gradeFrenteEsq);
+        predio.add(gradeFrenteDir);
+        gradeFrenteEsq.position.set(-5.5, -4 -3, -5 + i*3.2);
+        gradeFrenteDir.position.set(5.5, -4 -3, -5 + i*3.2);
+
+        var gradeLateral1 = new THREE.Mesh(gradeLateralGeometry, gradeMaterial);
+        var gradeLateral2 = new THREE.Mesh(gradeLateralGeometry, gradeMaterial);
+        predio.add(gradeLateral1);
+        predio.add(gradeLateral2);
+        gradeLateral1.position.set(-10 + 0.05, -4 - 1.5, -5 + i*3.2);
+        gradeLateral2.position.set(10 - 0.05, -4 - 1.5, -5 + i*3.2)
+
+        var pisoEsq = new THREE.Mesh(pisoGeometry, pisoMaterial);
+        var pisoDir = new THREE.Mesh(pisoGeometry, pisoMaterial);
+        predio.add(pisoEsq);
+        predio.add(pisoDir);
+        pisoEsq.position.set(-5.5, -4 - 1.5, -6 + 0.1 + i*3.2)
+        pisoDir.position.set(5.5, -4 - 1.5, -6 + 0.1 + i*3.2)
+
+        var portaVarandaEsq = new THREE.Mesh(portaVarandaGeometry, portaVarandaMaterial);
+        var portaVarandaDir = new THREE.Mesh(portaVarandaGeometry, portaVarandaMaterial2);
+        predio.add(portaVarandaEsq);
+        predio.add(portaVarandaDir);
+        portaVarandaEsq.rotateOnAxis(new THREE.Vector3(1,0,0), degreesToRadians(90));
+        portaVarandaDir.rotateOnAxis(new THREE.Vector3(1,0,0), degreesToRadians(90));
+        portaVarandaEsq.position.set(-1.5, -4.01, -6 + 2.5/2 + i*3.2)
+        portaVarandaDir.position.set(1.5, -4.01, -6 + 2.5/2 + i*3.2)
+        portaVarandaEsq.material.map.wrapS = THREE.RepeatWrapping;
+        portaVarandaEsq.material.map.repeat.x = -1;
+
+        var janelaEsq = new THREE.Mesh(janelaGeometry, janelaMaterial);
+        var janelaDir = new THREE.Mesh(janelaGeometry, janelaMaterial);
+        predio.add(janelaEsq);
+        predio.add(janelaDir);
+        janelaEsq.rotateOnAxis(new THREE.Vector3(0,1,0), degreesToRadians(-90));
+        janelaDir.rotateOnAxis(new THREE.Vector3(0,1,0), degreesToRadians(90));
+        janelaEsq.position.set(-10.01, 0, -4.5 + i*3.2)
+        janelaDir.position.set(10.01, 0, -4.5 + i*3.2)
+
+        var janelaFrenteEsq = new THREE.Mesh(janelaFrenteGeometry, janelaMaterial);
+        var janelaFrenteDir = new THREE.Mesh(janelaFrenteGeometry, janelaMaterial);
+        predio.add(janelaFrenteEsq);
+        predio.add(janelaFrenteDir);
+        janelaFrenteEsq.rotateOnAxis(new THREE.Vector3(1,0,0), degreesToRadians(90));
+        janelaFrenteDir.rotateOnAxis(new THREE.Vector3(1,0,0), degreesToRadians(90));
+        janelaFrenteEsq.position.set(-6, -4.01, -4 + i*3.2)
+        janelaFrenteDir.position.set(6, -4.01, -4 + i*3.2)
+    }
+
+    var porta = new THREE.Mesh(portaFrenteGeometry,portaFrenteMaterial);
+    predio.add(porta);
+    porta.rotateOnAxis(new THREE.Vector3(1,0,0), degreesToRadians(90));
+    porta.position.set(0,-4.01,-8);
+
+    return predio;
 }
 
 function render() {
