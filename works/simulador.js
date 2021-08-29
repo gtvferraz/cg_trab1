181231c9f35bfa90f88f2ce6dc1960fa31e976ab
guedes209 <bruno.guedes@ice.ufjf.br>
2021-08-29 18:23:49 -0300

    loader arrumado

--- works/simulador.js
+++ works/simulador.js
@@ -749,7 +749,7 @@ function render() {
         }
 
         LoadingManager.onLoad = function() {
-            loader.innerHTML = 'Aperte espaço para continuar';
+            loader.innerHTML = loader.innerHTML + '<br>Aperte espaço para continuar';
 
             render2();
             requestAnimationFrame(render2);
